2cd130b10306c7ed4b58853199995066ec01f66b
Laura-Martinez-Galindo <lf.martinezg1@uniandes.edu.co>
2025-11-19 08:02:49 -0500

    promedio

--- src/app/movie/movie-list/movie-list.component.ts
+++ src/app/movie/movie-list/movie-list.component.ts
@@ -10,6 +10,7 @@ import { MovieService } from '../movie.service';
   styleUrl: './movie-list.component.css',
 })
 export class MovieListComponent implements OnInit {
+  promedio: Number = 0;
   movies: Movie[] = [];
   selected: Boolean = false;
   selectedMovie: Movie | null = null;
@@ -18,19 +19,32 @@ export class MovieListComponent implements OnInit {
   getMoviesList(): Movie[]{
     this.movieService.getMovies().subscribe((data) => {
       this.movies = data;
+      this.calcularPromedio();
     });
     return this.movies;
   }
 
   ngOnInit() {
     this.getMoviesList();
+    this.calcularPromedio();
   }
 
   onSelect(movie: Movie) {
     this.selectedMovie = movie;
     this.selected = true;
   }
+
+  calcularPromedio(){
+    let total = 0;
+    let suma = 0;
+    for (let movie of this.movies) {
+      for (let actor of movie.cast){
+        total += 1;
+        suma += actor.popularity;
+      }
+      
+      
+    }
+    this.promedio = suma/total;
+  }
 }
-
-
-
