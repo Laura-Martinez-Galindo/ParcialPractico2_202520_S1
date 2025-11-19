22ca5f43008d979634ce5a55937b59ff48ae114d
Laura-Martinez-Galindo <lf.martinezg1@uniandes.edu.co>
2025-11-19 08:12:26 -0500

    listo duracion

--- src/app/movie/movie-detail/movie-detail.component.ts
+++ src/app/movie/movie-detail/movie-detail.component.ts
@@ -12,12 +12,14 @@ import { MovieService } from '../movie.service';
 export class MovieDetailComponent implements OnInit {
   movieId!: string; //! Confia en que se inicializa con un valor 
   @Input() movie: any;
+  duracion: string = '';
   constructor(private route:ActivatedRoute, private movieService:MovieService) {}
   
   //esto es obtener pelicula con el id si selecciono alguna vaina
   getMovie(){
     this.movieService.getMovie(this.movieId).subscribe(
       data => {this.movie = data;
+        this.getDuracion();
       })
   }
   ngOnInit() { //esto por si se busca una ruta y no se sleeciono la movie
@@ -26,8 +28,15 @@ export class MovieDetailComponent implements OnInit {
       if (this.movieId) {
         this.getMovie();
       }
+    } else{
+      this.getDuracion();
     }
     
+  }
+  getDuracion(){
+    let horas = this.movie.duration/60;
+    let minutos = this.movie.duration%60;
+    this.duracion = horas + 'H y ' + minutos + ' m';
   }
 }
 
